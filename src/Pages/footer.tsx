6d107337ab4f89e80166f24a0d59f7ba47ad6e34
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-04 12:31:32 +0530

    add footer

---
+++ src/Pages/footer.tsx
@@ -0,0 +1,8 @@
+
+export function Footer(){
+    return (
+        <footer className="fixed bottom-0 bg-[#0f3644] text-primaryCoral w-full  text-center p-4 font-semibold">
+            Made with love by Madhushree Kunder
+        </footer>
+    )
+}

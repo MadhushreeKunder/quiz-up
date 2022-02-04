6126f06ad6ff459905c2e30170b5554430c7f140
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-04 14:45:47 +0530

    add quizdb

---
+++ src/Pages/data/getQuiz.ts
@@ -0,0 +1,61 @@
+export const quizOne = {
+  quizName: "ISRO",
+  _id: "1",
+
+  questions: [
+    {
+      text: 'Who is known as the "Founding Father" of the Indian Space Programme?',
+      points: 5,
+      negativePoints: 2,
+      options: [
+        {
+          text: "A.P.J. Abdul Kalam",
+          isRight: false,
+        },
+        {
+          text: "Vikram Sarabhai",
+          isRight: true,
+        },
+        {
+          text: "Homi J. Bhabha",
+          isRight: false,
+        },
+        {
+          text: "Satish Dhawan",
+          isRight: false,
+        },
+      ],
+    },
+  ],
+};
+
+export const quizTwo = {
+  quizName: "Space and Stars",
+  _id: "2",
+
+  questions: [
+    {
+      text: "Which star other than the Sun is the closest to the Earth?",
+      points: 5,
+      negativePoints: 2,
+      options: [
+        {
+          text: "Proxima Centauri b",
+          isRight: false,
+        },
+        {
+          text: "Alpha Centauri",
+          isRight: false,
+        },
+        {
+          text: "Proxima Centauri",
+          isRight: true,
+        },
+        {
+          text: "Alpha Centauri A",
+          isRight: false,   
+        },
+      ],
+    },
+  ],
+};

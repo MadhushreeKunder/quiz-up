ef4525e7ebbc94752ea51a00f561692a24f90f19
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-04 13:28:14 +0530

    add quiz category page

---
+++ src/Pages/quizCategory.tsx
@@ -0,0 +1,32 @@
+import { Footer } from "./footer";
+
+export function QuizCategory() {
+  return (
+    <>
+      <div className="max-w-screen-xl w-full mt-32 mx-auto ">
+        <h1 className="font-semibold text-4xl text-secondaryDark mb-8 ">
+          Quiz category
+        </h1>
+        <div className="flex justify-center text-center items-center  space-x-8">
+          <div className="p-4 shadow-xl rounded-xl">
+            <h2 className="text-3xl font-medium text-secondaryDark mb-4 ">
+              ISRO
+            </h2>
+            <button className="text-xl bg-primaryCoral shadow-lg text-white rounded-full px-4 py-2 font-semibold text-center uppercase">
+              Start Quiz
+            </button>
+          </div>
+          <div className="p-4 shadow-xl rounded-xl">
+            <h2 className="text-3xl font-medium text-secondaryDark mb-4 ">
+              NASA
+            </h2>
+            <button className="text-xl bg-primaryCoral shadow-lg text-white rounded-full px-4 py-2 font-semibold text-center uppercase">
+              Start Quiz
+            </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}

30890e53b2db032ddba80f39037b18e8c811ca94
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-06 03:22:35 +0530

    export pages

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -3,3 +3,7 @@ export { QuizCategory } from "./quizCategory";
 export { QuizQuestions } from "./quizQuestionsCard";
 export { Login } from "./login";
 export { SignUp } from "./signup";
+export { QuizComp } from "./quiz";
+export { Result } from "./result";
+export {QuizBoard} from "./quizBoard";
+export { Rules } from "./rules";

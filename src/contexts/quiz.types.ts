b8d999783f78a47a9b7883ac41f936846bb49f76
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-05 17:57:23 +0530

    add quiz types

---
+++ src/contexts/quiz.types.ts
@@ -0,0 +1,17 @@
+import { Category, Quiz } from "../data/quizdb.types";
+import { QuizAction } from "../reducers/quiz.reducer.types";
+import { Status } from "./utils.types";
+
+
+export type ContextInititalState = {
+    quiz: Quiz[] | null;
+    quizDispatch: (action: QuizAction) => void;
+    categories: Category[] | null;
+    categoryQuizzes: Quiz[];
+    currentQuiz: null | Quiz;
+    score: number;
+    seconds: number | string;
+    showAnswer: boolean;
+    currentQuestionNo: number;
+    status: Status;
+}

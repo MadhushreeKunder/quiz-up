1ace397acc84db3b2ab56ddf0e71c83f58500f3e
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-05 18:25:14 +0530

    add getQuiz by category

---
+++ src/contexts/quizContext.tsx
@@ -0,0 +1,115 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import { Category } from "../data/quizdb.types";
+import { ContextInititalState } from "./quiz.types";
+import { InitialQuizState } from "../reducers/quiz.reducer.types";
+import { quizReducer } from "../reducers/quiz.reducer";
+import { Status } from "./utils.types";
+import { getCategories } from "./quiz.utils";
+import { getQuizzes } from "./quiz.utils";
+import { getQuizzesByCategory } from "../utils/utils";
+
+export const QuizContext = createContext<ContextInititalState>(
+  {} as ContextInititalState
+);
+
+export const initialQuizState: InitialQuizState = {
+  quizzes: null,
+  categories: null,
+  currentQuestionNo: 0,
+  score: 0,
+  seconds: 10,
+  viewByCategory: {} as Category,
+  showAnswer: false,
+  currentQuiz: null,
+  status: {} as Status,
+};
+
+export const QuizProvider = ({ children }) => {
+  useEffect(() => {
+    (async () => {
+      quizDispatch({
+        type: "SET_STATUS",
+        payload: { status: { loading: "Loading data from server" } },
+      });
+      const categories = await getCategories();
+      if (Array.isArray(categories)) {
+        quizDispatch({
+          type: "SET_STATUS",
+          payload: { status: { loading: "" } },
+        });
+        return quizDispatch({
+          type: "SET_CATEGORIES",
+          payload: { data: categories },
+        });
+      }
+      quizDispatch({
+        type: "SET_STATUS",
+        payload: { status: { error: categories } },
+      });
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      quizDispatch({
+        type: "SET_STATUS",
+        payload: { status: { loading: "Loading data from server" } },
+      });
+      const quizzes = await getQuizzes();
+      if (Array.isArray(quizzes)) {
+        quizDispatch({
+          type: "SET_STATUS",
+          payload: { status: { loading: "" } },
+        });
+        return quizDispatch({
+          type: "SET_QUIZZES",
+          payload: { data: quizzes },
+        });
+      }
+      quizDispatch({
+        type: "SET_STATUS",
+        payload: { status: { error: quizzes } },
+      });
+    })();
+  }, []);
+
+  const [
+    {
+      quizzes,
+      categories,
+      currentQuestionNo,
+      viewByCategory,
+      score,
+      currentQuiz,
+      seconds,
+      showAnswer,
+      status,
+    },
+    quizDispatch,
+  ] = useReducer(quizReducer, initialQuizState);
+
+  const categoryQuizzes = getQuizzesByCategory(quizzes, viewByCategory);
+
+  return (
+    <QuizContext.Provider
+      value={{
+        quizzes,
+        quizDispatch,
+        categories,
+        categoryQuizzes,
+        currentQuiz,
+        currentQuestionNo,
+        score,
+        seconds,
+        showAnswer,
+        status,
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+};
+
+export const useQuiz = () => {
+  return useContext<ContextInititalState>(QuizContext);
+}
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,9 @@
 import { InitialQuizState } from "../reducers/quiz.reducer.types";
 
+import { Category, Quiz } from "../data/quizdb.types";
+
+export const Backend_URL = "https://quizup-backend.madhushreekunde.repl.co";
+
 export const getScore = (state: InitialQuizState, action): number => {
   if (action.payload.answer.isRight) {
     if (state.currentQuiz !== null && state.currentQuiz.questions) {
@@ -11,14 +15,28 @@ export const getScore = (state: InitialQuizState, action): number => {
       return state.score;
     }
   } else {
-      if (state.currentQuiz !== null && state.currentQuiz.questions){
-          const question = state.currentQuiz.questions[action.payload.currentQuestionNo];
-          return question.negativePoints? state.score - question.negativePoints : 0;
-      } else {
-          return state.score;
-      }
+    if (state.currentQuiz !== null && state.currentQuiz.questions) {
+      const question =
+        state.currentQuiz.questions[action.payload.currentQuestionNo];
+      return question.negativePoints
+        ? state.score - question.negativePoints
+        : 0;
+    } else {
+      return state.score;
+    }
   }
 };
 
-
-export const Backend_URL = "https://quizup-backend.madhushreekunde.repl.co"
+export const getQuizzesByCategory = (
+  quizzes: Quiz[] | null,
+  viewByCategory: Category
+): Quiz[] => {
+  return quizzes
+    ? quizzes.filter((quiz) =>
+        Object.keys(viewByCategory).length === 0 &&
+        viewByCategory.constructor === Object
+          ? quiz
+          : quiz.categoryId._id === viewByCategory._id
+      )
+    : [];
+};

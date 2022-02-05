74891a8368bb3ce91bfd2db27e4c24af97ffd5a3
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-05 20:18:25 +0530

    add auth context

---
+++ src/contexts/authContext.tsx
@@ -0,0 +1,183 @@
+import axios, { AxiosError } from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Backend_URL } from "../utils/utils";
+import { ServorError } from "./utils.types";
+import {
+  InitialAuthState,
+  User,
+  LoginResponse,
+  SignupResponse,
+} from "./auth.types";
+import { Status } from "./utils.types";
+import {
+  localStorageHasItem,
+  setupAuthHeaderForServiceCalls,
+  setupAuthExceptionHandler,
+  setUpUser,
+} from "./auth.utils";
+
+export const AuthContext = createContext<InitialAuthState>(
+  {} as InitialAuthState
+);
+
+export const AuthProvider = ({ children }) => {
+  const { state } = useLocation();
+  let savedToken = localStorageHasItem("token");
+  if (savedToken) {
+    setupAuthHeaderForServiceCalls(savedToken);
+  }
+
+  const [token, setToken] = useState<string | null>(savedToken);
+  const [user, setUser] = useState<User>({
+    _id: "",
+    username: "",
+    email: "",
+  });
+
+  const [status, setStatus] = useState<Status>({
+    loading: "",
+    success: "",
+    error: {} as ServorError,
+  });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userFromLocalStorage = localStorage.getItem("user");
+    if (userFromLocalStorage) {
+      const userFromLocalStorageObj = JSON.parse(userFromLocalStorage);
+      setUser(userFromLocalStorageObj);
+    }
+    setupAuthExceptionHandler(logout, navigate);
+  }, []);
+
+  const loginUserWithCreds = async (username: string, password: string) => {
+    try {
+      setStatus({ loading: "Checking..   " } as Status);
+      const response = await axios.post<LoginResponse>(
+        `${Backend_URL}/auth/login`,
+        { username, password }
+      );
+
+      console.log({ response });
+      if (response.status === 200) {
+        setUpUser(
+          username,
+          response.data.user._id,
+          response.data.user.token,
+          response.data.user.email,
+          setUser,
+          setToken
+        );
+        setStatus({
+          success: `Login Successful. Welcome ${username}`,
+        } as Status);
+        navigate(state?.from ? state.from : "/");
+      }
+    } catch (error) {
+      console.error(error.response);
+      if (axios.isAxiosError(error)) {
+        const servorError = error as AxiosError<ServorError>;
+        if (servorError && servorError.response) {
+          return setStatus({
+            error: {
+              errorMessage: servorError.response.data.errorMessage,
+              errorCode: servorError.response.data.errorCode,
+            },
+          } as Status);
+        }
+      }
+      console.log(error.response);
+      setStatus({
+        error: {
+          errorMessage: "Something went wrong, try again!",
+          errorCode: 403,
+        },
+      } as Status);
+    }
+  };
+
+  const signUpUserWithCreds = async (
+    username: string,
+    password: string,
+    email: string
+  ) => {
+    try {
+      setStatus({ loading: "Wait.." } as Status);
+      const response = await axios.post<SignupResponse>(
+        `${Backend_URL}/auth/signup`,
+        { username, password, email }
+      );
+      console.log({ response });
+
+      if (response.status === 201) {
+        setUpUser(
+          username,
+          response.data.user._id,
+          response.data.user.token,
+          email,
+          setUser,
+          setToken
+        );
+        setStatus({
+          success: `Signup successful. Welcome ${username}`,
+        } as Status);
+        navigate(state?.from ? state.from : "/");
+      }
+    } catch (error) {
+      console.log(error.response);
+      if (axios.isAxiosError(error)) {
+        const servorError = error as AxiosError<ServorError>;
+        if (servorError && servorError.response) {
+          return setStatus({
+            error: {
+              errorMessage: servorError.response.data.errorMessage,
+              errorCode: servorError.response.data.errorCode,
+            },
+          } as Status);
+        }
+      }
+      console.log(error.response);
+      setStatus({
+        error: {
+          errorMessage: "Something went wrong, try again!",
+          errorCode: 403,
+        },
+      } as Status);
+    }
+  };
+
+  const logout = (): void => {
+    setToken("");
+    setStatus({ loading: "", success: "", error: {} as ServorError });
+
+    setUser({
+      _id: "",
+      username: "",
+      email: "",
+    });
+    localStorage?.removeItem("token");
+    localStorage?.removeItem("user");
+    navigate("/");
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        user,
+        status,
+        loginUserWithCreds,
+        signUpUserWithCreds,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext<InitialAuthState>(AuthContext);
+};

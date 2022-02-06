68c6c15a77cbc3b0532c0197505528ae3a3abeee
MadhushreeKunder <madhushreekunder@gmail.com>
2022-02-06 12:45:02 +0530

    add private route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import "./App.css";
 import { Home } from "./Pages/home";
 import { Header } from "./pageComponents/header";
 import { Route, Routes } from "react-router-dom";
-import { Login, QuizCategory, QuizComp, SignUp, Result, QuizBoard, Rules } from "./Pages";
+import { Login, QuizCategory, QuizComp, SignUp, Result, QuizBoard, Rules, Logout } from "./Pages";
+import { PrivateRoute } from "./pageComponents/privateRoute";
 
 
 function App() {
@@ -13,10 +14,17 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/quizzes" element={
+            <PrivateRoute>
+              <QuizCategory/>
+            </PrivateRoute>
+          }></Route>
+
           <Route path="/quizzes" element={<QuizCategory/>}></Route>
 
           <Route path="/login" element={<Login/>}></Route>
           <Route path="/signup" element={<SignUp/>}></Route>
+          <Route path="/logout" element={<Logout/>}></Route>
 
           <Route path="/rules/:quizId" element={<Rules/>}></Route>
           <Route path="/quizzes/:quizId" element={<QuizComp/>}></Route>
